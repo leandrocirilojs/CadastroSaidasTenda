7c5d3bbdc84590b121d31797d2aaa8b6730b9637
leandrocirilojs <48395196+leandrocirilojs@users.noreply.github.com>
2025-12-12 00:25:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -331,8 +331,8 @@ function exportToExcel() {
         expense.km,
         expense.nfs, //Qtd NFs
         expense.received,  // Valor Recebido
-        expense.profit,    // Lucro
-        expense.amount,      // Valor Pago
+    // expense.profit,    // Lucro
+      //  expense.amount,      // Valor Pago
         expense.weight,    // Peso (kg)
         expense.nfs,
         
@@ -353,7 +353,7 @@ function exportToExcel() {
 
     // Cabeçalho da planilha
     const ws = XLSX.utils.aoa_to_sheet([
-        ['TRANSPORTADOR MOTORISTA', 'DATA SAIDA', 'LOJA SAÍDA', 'INFORMAÇÕES ADICIONAIS  (NOTAS ADICIONAIS E SAIDAS A MAIS)', 'PEDAGIO ESTACIONAMENTO R$', 'KM EXCEDIDOS  R$', 'NOTAS E VIAGENS', 'VALOR PAGO', 'VALOR RECEBIDO', 'LUCRO', 'PESO (kg)', 'QTD NFs'],
+        ['TRANSPORTADOR MOTORISTA', 'DATA SAIDA', 'LOJA SAÍDA', 'INFORMAÇÕES ADICIONAIS  (NOTAS ADICIONAIS E SAIDAS A MAIS)', 'PEDAGIO ESTACIONAMENTO R$', 'KM EXCEDIDOS  R$', 'NOTAS E VIAGENS ADICIONAIS', 'FRETE EMBARCADO', 'PESO (kg)', 'QTD NFs'],
         ...data
     ]);
 
